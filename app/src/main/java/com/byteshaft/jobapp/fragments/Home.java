9e83c0749ff44356d6d96916a1547b463b31ab63
rb-shahid <shgr8rb@gmail.com>
2017-05-30 13:00:09 +0500

    add ui

--- app/src/main/java/com/byteshaft/jobapp/fragments/Home.java
+++ app/src/main/java/com/byteshaft/jobapp/fragments/Home.java
@@ -5,20 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.byteshaft.jobapp.R;
 
 import ss.com.bannerslider.banners.DrawableBanner;
 import ss.com.bannerslider.views.BannerSlider;
 
-/**
- * Created by shahid on 28/05/2017.
- */
 
-public class Home extends Fragment {
+public class Home extends Fragment implements View.OnClickListener {
 
     private View mBaseView;
     private BannerSlider bannerSlider;
+    private Button partTime;
+    private Button fullTime;
+    private Button internShip;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +30,26 @@ public class Home extends Fragment {
         bannerSlider.addBanner(new DrawableBanner(R.drawable.slide_imge));
         bannerSlider.addBanner(new DrawableBanner(R.drawable.slide_imge));
 
+        partTime = (Button) mBaseView.findViewById(R.id.button_part_time);
+        fullTime = (Button) mBaseView.findViewById(R.id.button_full_time);
+        internShip = (Button) mBaseView.findViewById(R.id.button_internship);
+
+        partTime.setOnClickListener(this);
+        fullTime.setOnClickListener(this);
+        internShip.setOnClickListener(this);
+
         return mBaseView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_part_time:
+                break;
+            case R.id.button_full_time:
+                break;
+            case R.id.button_internship:
+                break;
+        }
     }
 }
