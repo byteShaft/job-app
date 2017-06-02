5b3a54ad0664c1d3cc33b073c3258a38b9417c9d
hsunain hussi <husnain446@gmail.com>
2017-06-02 11:55:59 +0500

    add settings screen

--- app/src/main/java/com/byteshaft/jobapp/ProfileActivities/Settings.java
+++
@@ -1,8 +0,0 @@
-package com.byteshaft.jobapp.ProfileActivities;
-
-/**
- * Created by husnain on 6/1/17.
- */
-
-public class Settings {
-}
---
+++ app/src/main/java/com/byteshaft/jobapp/profile/profileSettings.java
@@ -0,0 +1,23 @@
+
+package com.byteshaft.jobapp.profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by husnain on 6/1/17.
+ */
+
+public class profileSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.byteshaft.jobapp.R.layout.activity_settings);
+        getSupportActionBar().setTitle("Settings");
+    }
+
+}
+
+
