faedbadbaabd525dbe08785a58ab7cfa80538995
hsunain hussi <husnain446@gmail.com>
2017-06-06 13:05:16 +0500

    search delegate

--- app/src/main/java/com/byteshaft/jobapp/fragments/Search.java
+++ app/src/main/java/com/byteshaft/jobapp/fragments/Search.java
@@ -3,6 +3,7 @@ package com.byteshaft.jobapp.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ public class Search extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBaseView = inflater.inflate(R.layout.fragment_search, container, false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         return mBaseView;
     }
 }
