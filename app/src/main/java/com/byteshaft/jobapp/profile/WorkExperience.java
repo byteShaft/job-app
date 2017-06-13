2f788c383ec0cbef000c643b0740ecf7519b3a9e
hsunain hussi <husnain446@gmail.com>
2017-06-13 18:06:36 +0500

    add methods

--- app/src/main/java/com/byteshaft/jobapp/profile/WorkExperience.java
+++ app/src/main/java/com/byteshaft/jobapp/profile/WorkExperience.java
@@ -9,14 +9,23 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.byteshaft.jobapp.R;
+import com.byteshaft.jobapp.utils.AppGlobals;
+import com.byteshaft.jobapp.utils.Helpers;
+import com.byteshaft.requests.HttpRequest;
 
-public class WorkExperience extends AppCompatActivity implements View.OnClickListener {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WorkExperience extends AppCompatActivity implements View.OnClickListener,
+        HttpRequest.OnReadyStateChangeListener, HttpRequest.OnErrorListener{
 
 
 
     private TextView buttonSave;
     private Toolbar toolbarTop;
     private ImageButton backButton;
+
+    private HttpRequest request;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,5 +50,37 @@ public class WorkExperience extends AppCompatActivity implements View.OnClickLis
                 System.out.println("save");
                 break;
         }
+    }
+
+    private void AddEducation(String company, String period, String title) {
+        request = new HttpRequest(this);
+        request.setOnReadyStateChangeListener(this);
+        request.setOnErrorListener(this);
+        request.open("POST", String.format("%sexperience/ ", AppGlobals.BASE_URL));
+        request.send(getWorkExperienceData(company, period, title));
+        Helpers.showProgressDialog(WorkExperience.this, "Saving Experiences..");
+    }
+
+    private String getWorkExperienceData(String company, String period, String title) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("company", company);
+            jsonObject.put("period", period);
+            jsonObject.put("title", title);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+
+    }
+
+    @Override
+    public void onError(HttpRequest request, int readyState, short error, Exception exception) {
+
+    }
+
+    @Override
+    public void onReadyStateChange(HttpRequest request, int readyState) {
+
     }
 }
