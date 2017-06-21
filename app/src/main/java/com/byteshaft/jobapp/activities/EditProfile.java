26f4a09d30470ac5ecd8b57813907303723bf9f9
hsunain hussi <husnain446@gmail.com>
2017-06-21 14:01:04 +0500

    add backbutton

--- app/src/main/java/com/byteshaft/jobapp/activities/EditProfile.java
+++ app/src/main/java/com/byteshaft/jobapp/activities/EditProfile.java
@@ -27,6 +27,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -74,6 +75,7 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
     private TextView mLocationTextView;
     private EditText mAddress;
     private CircleImageView mProfilePicture;
+    private ImageView mBackButton;
 
     private File destination;
     private Uri selectedImageUri;
@@ -107,11 +109,13 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         mAddress = (EditText) findViewById(R.id.location_edit_text);
         mLocationTextView = (TextView) findViewById(R.id.pick_for_current_location);
         mSaveButton = (Button) findViewById(R.id.button_save);
+        mBackButton = (ImageView) findViewById(R.id.back_button);
         mProfilePicture = (CircleImageView) findViewById(R.id.user_dp);
 
         mSaveButton.setOnClickListener(this);
         mProfilePicture.setOnClickListener(this);
         mLocationTextView.setOnClickListener(this);
+        mBackButton.setOnClickListener(this);
 
         if (imageUrl.trim().isEmpty() && imageUrl != null) {
             profilePic = Helpers.getBitMapOfProfilePic(imageUrl);
@@ -165,6 +169,9 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
                 mMobileNumberString = mMobileNumber.getText().toString();
                 mUserNameString = mUserName.getText().toString();
                 editProfileDataToServer();
+                break;
+            case R.id.back_button:
+                onBackPressed();
                 break;
         }
 
