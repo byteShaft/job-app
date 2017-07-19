d8ff9845048593d92c14031f93a8b063fb73692f
hsunain hussi <husnain446@gmail.com>
2017-07-19 18:08:31 +0500

    implement filter

--- app/src/main/java/com/byteshaft/jobapp/fragments/Filter.java
+++ app/src/main/java/com/byteshaft/jobapp/fragments/Filter.java
@@ -3,13 +3,17 @@ package com.byteshaft.jobapp.fragments;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -17,17 +21,48 @@ import android.widget.TextView;
 import com.byteshaft.jobapp.MainActivity;
 import com.byteshaft.jobapp.R;
 import com.byteshaft.jobapp.accounts.AccountManager;
+import com.byteshaft.jobapp.activities.FilterCategories;
 import com.byteshaft.jobapp.activities.FilterLocation;
+import com.byteshaft.jobapp.gettersetters.JobDetails;
 import com.byteshaft.jobapp.profile.ProfileSettings;
+import com.byteshaft.jobapp.utils.AppGlobals;
+import com.byteshaft.jobapp.utils.Helpers;
+import com.byteshaft.requests.HttpRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
-public class Filter extends Fragment implements View.OnClickListener{
+public class Filter extends Fragment implements View.OnClickListener {
 
     private View mBaseView;
 
     private ImageButton mBackButton;
     private TextView mResetTextView;
+    private static TextView mJobLocationTextView;
+    private static TextView mJobCategoryTextView;
     private RelativeLayout mLocationLayout;
+    private RelativeLayout mCategoriesLayout;
+
+    private Button mAnyButton;
+    private Button mFullTimeButton;
+    private Button mPartTimeButton;
+    private Button mInternshipButton;
+    private Button mApplyFilter;
+
+    private String mButtonText;
+    private static String mJobLocationTextViewString;
+    private static String mJobCategoryTextViewString;
+
+    private ArrayList<JobDetails> jobsArrayList;
+    private JobListAdapter adapter;
+
 
     @Nullable
     @Override
@@ -35,12 +70,41 @@ public class Filter extends Fragment implements View.OnClickListener{
         mBaseView = inflater.inflate(R.layout.fragment_filter, container, false);
         mBackButton = (ImageButton) mBaseView.findViewById(R.id.back_button);
         mResetTextView = (TextView) mBaseView.findViewById(R.id.reset_text_view);
+        mJobLocationTextView = (TextView) mBaseView.findViewById(R.id.job_location_text_view);
+        mJobCategoryTextView = (TextView) mBaseView.findViewById(R.id.jobs_category_text_view);
         mLocationLayout = (RelativeLayout) mBaseView.findViewById(R.id.location_layout);
+        mCategoriesLayout = (RelativeLayout) mBaseView.findViewById(R.id.categories_layout);
+        mAnyButton = (Button) mBaseView.findViewById(R.id.button_any);
+        mFullTimeButton = (Button) mBaseView.findViewById(R.id.button_full_time);
+        mPartTimeButton = (Button) mBaseView.findViewById(R.id.button_part_time);
+        mInternshipButton = (Button) mBaseView.findViewById(R.id.button_internship);
+        mApplyFilter = (Button) mBaseView.findViewById(R.id.apply_filter);
 
         mBackButton.setOnClickListener(this);
         mResetTextView.setOnClickListener(this);
         mLocationLayout.setOnClickListener(this);
+        mCategoriesLayout.setOnClickListener(this);
+
+        mAnyButton.setOnClickListener(this);
+        mFullTimeButton.setOnClickListener(this);
+        mPartTimeButton.setOnClickListener(this);
+        mInternshipButton.setOnClickListener(this);
+        mApplyFilter.setOnClickListener(this);
+        jobsArrayList = new ArrayList<>();
+        setTextForLocation(AppGlobals.getStringFromSharedPreferences(AppGlobals.KEY_JOB_LOCATION_NAME));
+        setTextForCategory(AppGlobals.getStringFromSharedPreferences(AppGlobals.KEY_JOB_CATEGORY_NAME));
         return mBaseView;
+    }
+
+    public static void setTextForLocation(String value) {
+        mJobLocationTextView.setText(value);
+        mJobLocationTextViewString = mJobLocationTextView.getText().toString();
+
+    }
+
+    public static void setTextForCategory(String value) {
+        mJobCategoryTextView.setText(value);
+        mJobCategoryTextViewString = mJobCategoryTextView.getText().toString();
     }
 
     @Override
@@ -57,6 +121,7 @@ public class Filter extends Fragment implements View.OnClickListener{
                         .setCancelable(false).setPositiveButton("Yes",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
+                                AppGlobals.clearSettings();
                                 dialog.dismiss();
                             }
                         });
@@ -72,6 +137,140 @@ public class Filter extends Fragment implements View.OnClickListener{
             case R.id.location_layout:
                 startActivity(new Intent(getActivity(), FilterLocation.class));
                 break;
+            case R.id.categories_layout:
+                startActivity(new Intent(getActivity(), FilterCategories.class));
+                break;
+            case R.id.button_any:
+                mButtonText = ((Button) v).getText().toString();
+                break;
+            case R.id.button_full_time:
+                mButtonText = ((Button) v).getText().toString();
+                break;
+            case R.id.button_part_time:
+                mButtonText = ((Button) v).getText().toString();
+                break;
+            case R.id.button_internship:
+                mButtonText = ((Button) v).getText().toString();
+                break;
+            case R.id.apply_filter:
+                getJobsList(mButtonText, mJobCategoryTextViewString, mJobLocationTextViewString);
+                System.out.println();
+                break;
         }
+    }
+
+    private void getJobsList(String jobType, String category, String location) {
+        HttpRequest request = new HttpRequest(AppGlobals.getContext());
+        Helpers.showProgressDialog(getActivity(), "Please wait...");
+        request.setOnReadyStateChangeListener(new HttpRequest.OnReadyStateChangeListener() {
+            @Override
+            public void onReadyStateChange(HttpRequest request, int readyState) {
+                switch (readyState) {
+                    case HttpRequest.STATE_DONE:
+                        Helpers.dismissProgressDialog();
+                        switch (request.getStatus()) {
+                            case HttpURLConnection.HTTP_OK:
+                                Log.i("MY Category", request.getResponseText());
+                                try {
+                                    JSONArray jsonArray = new JSONArray(request.getResponseText());
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                        JobDetails jobDetails = new JobDetails();
+                                        jobDetails.setJobType(jsonObject.getString("type"));
+                                        jobDetails.setJobTitle(jsonObject.getString("title"));
+                                        jobDetails.setScope(jsonObject.getString("scope"));
+                                        jobDetails.setSalary(jsonObject.getString("salary"));
+                                        jobDetails.setRequirement(jsonObject.getString("requirement"));
+                                        jobDetails.setLocation(jsonObject.getString("location"));
+                                        jobDetails.setDetailDescription(jsonObject.getString("detailed_description"));
+                                        jobsArrayList.add(jobDetails);
+                                    }
+                                    adapter = new JobListAdapter(jobsArrayList);
+//                                    listView.setAdapter(adapter);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                        }
+                }
+            }
+        });
+        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+            @Override
+            public void onError(HttpRequest request, int readyState, short error, Exception exception) {
+                Helpers.dismissProgressDialog();
+                switch (readyState) {
+                    case HttpRequest.ERROR_CONNECTION_TIMED_OUT:
+                        Helpers.showSnackBar(getView(), "connection time out");
+                        break;
+                    case HttpRequest.ERROR_NETWORK_UNREACHABLE:
+                        Helpers.showSnackBar(getView(), exception.getLocalizedMessage());
+                        break;
+                }
+            }
+        });
+        request.open("GET", String.format("%sjobs/?type=%s&category=%s&location=%s", AppGlobals.BASE_URL, jobType, category, location));
+        request.send();
+    }
+
+    private class JobListAdapter extends BaseAdapter {
+        private ViewHolder viewHolder;
+        private ArrayList<JobDetails> jobDetails;
+
+
+        public JobListAdapter(ArrayList<JobDetails> jobDetails) {
+            this.jobDetails = jobDetails;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.delegate_jobs_list, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.companyName = (TextView) convertView.findViewById(R.id.company_name);
+                viewHolder.jobTitle = (TextView) convertView.findViewById(R.id.job_title);
+                viewHolder.jobLocation = (TextView) convertView.findViewById(R.id.company_location);
+//                viewHolder.companyLogo = (CircleImageView) convertView.findViewById(R.id.job_icon);
+                viewHolder.salary = (TextView) convertView.findViewById(R.id.salary);
+                viewHolder.jobCategory = (TextView) convertView.findViewById(R.id.part_time_button);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            JobDetails jobDetail = jobDetails.get(position);
+            viewHolder.companyName.setText(jobDetail.getJobTitle());
+            viewHolder.jobTitle.setText(jobDetail.getJobTitle());
+            viewHolder.jobLocation.setText(jobDetail.getLocation());
+            viewHolder.salary.setText(jobDetail.getSalary());
+            viewHolder.jobCategory.setText(jobDetail.getJobType());
+
+            return convertView;
+        }
+
+        @Override
+        public int getCount() {
+            return jobDetails.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+    }
+
+    class ViewHolder {
+        private TextView companyName;
+        private TextView jobTitle;
+        private TextView jobLocation;
+        private CircleImageView companyLogo;
+        private TextView salary;
+        private TextView jobCategory;
+
     }
 }
