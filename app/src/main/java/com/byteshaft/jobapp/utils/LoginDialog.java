e04d69ec548d50450d582e07593da948d2c3b195
rb-shahid <shgr8rb@gmail.com>
2017-08-01 12:59:48 +0500

    add dialog class and xml

---
+++ app/src/main/java/com/byteshaft/jobapp/utils/LoginDialog.java
@@ -0,0 +1,45 @@
+package com.byteshaft.jobapp.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.byteshaft.jobapp.R;
+import com.byteshaft.jobapp.accounts.AccountManager;
+
+public class LoginDialog extends Dialog implements View.OnClickListener {
+
+    private Button registerButton;
+    private TextView loginButton;
+
+    public LoginDialog(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_login);
+        registerButton = (Button) findViewById(R.id.dialog_register_button);
+        loginButton = (TextView) findViewById(R.id.login_text_button);
+        registerButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.dialog_register_button:
+                System.out.println("OOKOK");
+                break;
+            case R.id.login_text_button:
+                Intent i = new Intent(getContext(), AccountManager.class);
+                getContext().startActivity(i);
+                break;
+        }
+    }
+}
