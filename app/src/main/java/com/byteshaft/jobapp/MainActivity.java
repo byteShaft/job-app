0b13af20e613de3d9de2ba8463d4b9ab8fca2d55
rb-shahid <shgr8rb@gmail.com>
2017-08-01 14:15:38 +0500

    make dialog functional and add missing string file

--- app/src/main/java/com/byteshaft/jobapp/MainActivity.java
+++ app/src/main/java/com/byteshaft/jobapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.byteshaft.jobapp;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -41,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        public boolean onNavigationItemSelected(MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     loadThisFragment(new Home(), "", "");
